de933877575a45e5cac72eb4ae4673a3dfe54649
Mario Usanga <mariobomausanga@gmail.com>
2026-06-01 12:18:34 +1000

    Created a Dash app which visualizes pink morsel sales per day

---
+++ app.py
@@ -0,0 +1,24 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+import os
+
+app = Dash()
+
+file_path = os.path.join('data', 'single_formatted_output.csv')
+
+df = pd.read_csv(file_path)
+
+fig = px.line(df, x="Date", y="Sales", title="Pink Morsel Sales per Day")
+
+app.layout = html.Div(children=[
+    # html.H1(children='Pink Morsel Sales'),
+    dcc.Graph(
+        id='sales-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
